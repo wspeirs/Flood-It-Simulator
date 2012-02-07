fc5d2bb8e39fd1e415620830f05ece3f65c388b5
William R. Speirs <bill.speirs@gmail.com>
2012-02-06 19:13:21 -0500

    - Integrated Barry's changes   - New algorithm to flood board   - Removed old algorithm and associated methods

--- src/main/java/com/bittrust/floodit/gui/Board.java
+++ src/main/java/com/bittrust/floodit/gui/Board.java
@@ -64,39 +64,42 @@ public class Board extends JPanel {
      * @return The number of new squares converted.
      */
     public int flood(Color color) {
-        int changeCount = 0;
-        
         Color[][] newBoard = new Color[HEIGHT][WIDTH];
         
-        for(int h=0; h < HEIGHT; ++h) {
-            for(int w=0; w < WIDTH; ++w) {
-                Color c = board[h][w].getBackground();
-                
-                if(c.equals(currentColor) && reachHome(w,h)) {
-                    newBoard[h][w] = color;
-                } else if(c.equals(color) && 
-                        ( reachHome(w-1,h) ||
-                          reachHome(w,h-1) ||
-                          reachHome(w+1,h) ||
-                          reachHome(w,h+1))) {
-                    changeCount++;
-                    newBoard[h][w] = c;
-                } else {
-                    newBoard[h][w] = c;
-                }
-            }
-        }
+        int total = flood( color, 0, 0, newBoard, currentColor );       
         
         currentColor = color;
         setBoard(newBoard);
         
-        return changeCount;
+        return total;
     }
-    
+
+    private int flood(Color color, int row, int column, Color[][] newBoard, Color parentColor ) {
+        if( row >= 0 && row < HEIGHT && column >= 0 && column < WIDTH && newBoard[row][column] == null ) {
+            Color currentColor = board[row][column].getBackground();
+            int count =  0;
+            
+            if( currentColor.equals( parentColor ) ) {          
+                newBoard[row][column] = color;
+                count++;
+            
+                count +=    flood( color, row + 1, column, newBoard, currentColor ) +           
+                            flood( color, row - 1, column, newBoard, currentColor ) +
+                            flood( color, row, column + 1, newBoard, currentColor ) +
+                            flood( color, row, column - 1, newBoard, currentColor );
+            }
+                        
+            return count;
+        }
+        else
+            return 0;   
+    }
+
     public void setBoard(Color[][] colors) {
         for(int h=0; h < HEIGHT; ++h) {
             for(int w=0; w < WIDTH; ++w) {
-                board[h][w].setBackground(colors[h][w]);
+                if(colors[h][w] != null)
+                    board[h][w].setBackground(colors[h][w]);
             }
         }
         
@@ -115,36 +118,6 @@ public class Board extends JPanel {
         return ret;
     }
     
-    private boolean reachHome(int x, int y) {
-        return reachHome(x, y, getColorBoard());
-    }
-    
-    private boolean reachHome(int x, int y, Color[][] b) {
-        if(x == 0 && y == 0)
-            return true;
-        
-        if(x <= -1 || y <= -1 || x >= WIDTH || y >= HEIGHT)
-            return false;
-        
-        Color c = new Color(b[y][x].getRGB());
-        
-        b[y][x] = Color.BLACK;
-
-        if(y > 0 && b[y-1][x].equals(c))
-            return reachHome(x, y-1, b);
-        
-        if(x > 0 && b[y][x-1].equals(c))
-            return reachHome(x-1, y, b);
-        
-        if(y < HEIGHT-1 && b[y+1][x].equals(c))
-            return reachHome(x, y+1, b);
-        
-        if(x < WIDTH-1 && b[y][x+1].equals(c))
-            return reachHome(x+1,y, b);
-        
-        return false;
-    }
-   
     private JLabel generateLabel(Color color) {
         JLabel ret = new JLabel();
         
